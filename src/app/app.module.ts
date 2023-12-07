c3c54809f60375aa5e47d4ef401402a2b9f58fad
Alexandre Maurice <hizaaknewton@gmail.com>
2023-12-07 23:34:31 +0100

    add navbar and other page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { CompteurComponent } from './pages/home/components/compteur/compteur.component';
 import { HomeComponent } from './pages/home/home.component';
+import { CompteurComponent } from './pages/home/components/compteur/compteur.component';
+import { QuestionnaireComponent } from './pages/questionnaire/questionnaire.component';
+import { InfosComponent } from './pages/infos/infos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HomeComponent,
     CompteurComponent,
-    HomeComponent
+    QuestionnaireComponent,
+    InfosComponent
   ],
   imports: [
     BrowserModule
