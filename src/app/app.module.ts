1d9bb0becfdd7c6841388bb0875f9bb8ef33230c
neoelduayen <nelduayen@univ-pau.fr>
2023-12-07 23:23:27 +0100

    Création de la première page de la page d'accueil

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CompteurComponent } from './pages/home/components/compteur/compteur.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CompteurComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
