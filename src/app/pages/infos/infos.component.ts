21f1b0dd62dcda31406032217a19f95010a41caa
Pierre <pierredavid640@gmail.com>
2023-12-08 03:13:48 +0100

    Fact1

--- src/app/pages/infos/infos.component.ts
+++ src/app/pages/infos/infos.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./infos.component.scss']
 })
 export class InfosComponent {
-    public list:{title:string, content:string}[] = [{title:"Titre", content:"Contenu"}, {title:"Titre2", content:"Contenu2"},{title:"Titre3", content:"Contenu3"},{title:"Titre4", content:"Contenu4"}];
+    public list:{title:string, content:string}[] = [{title:"Ça fais beaucoup là non ?", content:"Chaque année nous perdon environ 1.2 Trillions de tonnes de glaces. Cela represante environ 1 224 489 800 000 000 000 Clio 2 ou 106 fois la masse de la Lune"}, {title:"Titre2", content:"Contenu2"},{title:"Titre3", content:"Contenu3"},{title:"Titre4", content:"Contenu4"}];
 }
